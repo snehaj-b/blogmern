17c212ed6c6234a177db7cebdcc2f9a146624372
Sanket Naikwade <your_email@example.com>
2026-05-14 14:04:19 +0530

    third

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = process.env.REACT_APP_API_URL || "http://3.25.237.192:5004/api";
+const API = process.env.REACT_APP_API_URL || "http://(localhost):5004/api";
 
 const styles = {
   header: {
